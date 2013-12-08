a9910343a81901931c912bb7b53f5504fabebe66
kawahiro <hiro.kawasaki0311@gmail.com>
2013-12-08 19:29:47 +0900

    日本語の文字化けを修正

--- .emacs.d/inits/00-init.el
+++ .emacs.d/inits/00-init.el
@@ -1,14 +1,12 @@
 ;;文字コード設定
-(setq default-buffer-file-coding-system 'utf-8-unix)
-(set-buffer-file-coding-system 'utf-8-unix)
-(set-terminal-coding-system 'utf-8-unix)
-(set-keyboard-coding-system 'utf-8-unix)
-(set-clipboard-coding-system 'utf-8-unix)
-(prefer-coding-system 'utf-8-unix)
-(set-language-environment 'utf-8)
-(set-default-coding-systems 'utf-8-unix)
+(set-locale-environment nil)
 (set-language-environment "Japanese")
-(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(setq default-buffer-file-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(prefer-coding-system 'utf-8)
 
 ;; file名の補完で大文字小文字を区別しない
 (setq completion-ignore-case t)
