43029cb827a0b7dac9cd7a6863a7d5090a9596ed
kawahiro311 <hiro.kawasaki0311@gmail.com>
2014-02-07 16:47:14 +0900

    Guardfile Rakefile config.ru をruby-modeで

--- .emacs.d/inits/20-ruby.el
+++ .emacs.d/inits/20-ruby.el
@@ -3,6 +3,9 @@
 (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("config.ru$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 
 (add-hook 'ruby-mode-hook
