b57bd8adf2880e1e3ab48be11fcd089d639dc372
kawahiro311 <hiro.kawasaki0311@gmail.com>
2014-03-20 20:22:21 +0900

    Add chord

--- .emacs.d/inits/10-keybinds.el
+++ .emacs.d/inits/10-keybinds.el
@@ -1,3 +1,7 @@
+;;key-chord
+(require 'key-chord)
+(key-chord-mode 1)
+
 ;;delete
 (global-set-key (kbd "C-h") 'delete-backward-char)
 
@@ -6,3 +10,5 @@
 
 ;;"C-t" でウィンドウ切り替え
 (define-key global-map (kbd "C-t") 'other-window)
+
+(key-chord-define-global "hj" 'helm-ls-git-ls)
