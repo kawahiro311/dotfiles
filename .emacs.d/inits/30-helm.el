7bd70c88a393b5bc47bf10648cffe735f9579710
kawahiro311 <hiro.kawasaki0311@gmail.com>
2014-03-20 20:20:11 +0900

    Add helm global set key

--- .emacs.d/inits/30-helm.el
+++ .emacs.d/inits/30-helm.el
@@ -2,3 +2,7 @@
 (require 'helm-config)
 (require 'helm-ls-git)
 (helm-mode 1)
+
+(global-set-key (kbd "C-c f") 'helm-imenu)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
+(global-set-key (kbd "M-1") 'helm-ls-git-ls)
