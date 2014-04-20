352aff80508e1ee4d72bb683cf6a550db917129b
kawahiro311 <hiro.kawasaki0311@gmail.com>
2014-04-20 22:24:05 +0900

    Helm find-fileを無効に

--- .emacs.d/inits/30-helm.el
+++ .emacs.d/inits/30-helm.el
@@ -3,6 +3,8 @@
 (require 'helm-ls-git)
 (helm-mode 1)
 
+(add-to-list 'helm-completing-read-handlers-alist '(find-file . nil))
+
 (global-set-key (kbd "C-c f") 'helm-imenu)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "M-1") 'helm-ls-git-ls)
