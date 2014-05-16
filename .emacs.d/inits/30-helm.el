4062653f2c12430f16fae2f7b3d6abcdf73e340d
kawahiro311 <hiro.kawasaki0311@gmail.com>
2014-05-16 11:40:45 +0900

    helmの設定

--- .emacs.d/inits/30-helm.el
+++ .emacs.d/inits/30-helm.el
@@ -3,8 +3,9 @@
 (require 'helm-ls-git)
 (helm-mode 1)
 
-(add-to-list 'helm-completing-read-handlers-alist '(find-file . nil))
-
+(global-set-key (kbd "C-q") 'helm-mini)
 (global-set-key (kbd "C-c f") 'helm-imenu)
+(global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "M-1") 'helm-ls-git-ls)
+(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
