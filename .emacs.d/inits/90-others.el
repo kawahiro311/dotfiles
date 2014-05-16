87953ee1bf7788873be09c6b14b4c429a12e74ed
kawahiro311 <hiro.kawasaki0311@gmail.com>
2014-05-16 18:17:45 +0900

    anzuの導入

--- .emacs.d/inits/90-others.el
+++ .emacs.d/inits/90-others.el
@@ -1,3 +1,11 @@
 ;;undo-tree
 (require 'undo-tree)
 (global-undo-tree-mode t)
+
+;;anzu
+(global-anzu-mode +1)
+
+(custom-set-variables
+ '(anzu-mode-lighter "")
+ '(anzu-deactivate-region t)
+  '(anzu-search-threshold 1000))
