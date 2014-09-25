8f19b40249b20f99021d2bfe6cb7a06c37177169
kawahiro311 <hiro.kawasaki0311@gmail.com>
2014-09-25 17:08:44 +0900

    helm gtagsの設定を追加

--- .emacs.d/inits/30-helm.el
+++ .emacs.d/inits/30-helm.el
@@ -1,6 +1,7 @@
 (require 'helm)
 (require 'helm-config)
 (require 'helm-ls-git)
+(require 'helm-gtags)
 (helm-mode 1)
 
 (global-set-key (kbd "C-q") 'helm-mini)
@@ -9,3 +10,12 @@
 (global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (global-set-key (kbd "M-1") 'helm-ls-git-ls)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
+
+;; tags
+(add-hook 'ruby-mode-hook (lambda () (helm-gtags-mode)))
+(add-hook 'helm-gtags-mode-hook
+	  '(lambda ()
+	     (local-set-key (kbd "M-g") 'helm-gtags-dwim)
+	     (local-set-key (kbd "M-s") 'helm-gtags-show-stack)
+	     (local-set-key (kbd "M-p") 'helm-gtags-previous-history)
+	     (local-set-key (kbd "M-n") 'helm-gtags-next-history)))
