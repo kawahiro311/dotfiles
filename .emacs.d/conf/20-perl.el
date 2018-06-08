d39abbc081b15584dedd9adfac7af61175a789f4
hiro-kawasaki <hiro.kawasaki@linecorp.com>
2018-06-08 19:37:21 +0900

    Update perl setting

--- .emacs.d/conf/20-perl.el
+++ .emacs.d/conf/20-perl.el
@@ -4,6 +4,9 @@
 (add-to-list 'auto-mode-alist '("\\.psgi$" . cperl-mode))
 (add-to-list 'auto-mode-alist '("cpanfile" . cperl-mode))
 
+;; indent
+(setq cperl-indent-parens-as-block t)
+
 ;; helm-perldoc
 (require 'helm-perldoc)
 (add-hook 'cperl-mode-hook 'helm-perldoc:carton-setup)
